d0a089d5bbd95a169e1e5835f3c40e5f7d708174
Lorena Muralha <mlmuralha2301@gmail.com>
2024-03-18 16:13:32 -0300

    First commit

---
+++ src/main/java/com/kingcode/demo/GorillaGrooveAppApplication.java
@@ -0,0 +1,13 @@
+package com.kingcode.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorillaGrooveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorillaGrooveAppApplication.class, args);
+	}
+
+}
